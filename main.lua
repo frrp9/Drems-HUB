854ade0a12237d59dbb36a578246c900435da703
frrp9 <121900262+frrp9@users.noreply.github.com>
2023-04-21 10:34:28 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,36 +9,30 @@ local function findGameId()
     -- Insérez ici le code pour effectuer la recherche de l'ID de jeu, par exemple :
     local gameId = "2753915549" -- Exemple d'ID trouvé
 
-    -- Retourne l'ID trouvé ou nil s'il n'a pas été trouvé
+    -- Retoune l'ID trouvé ou nil s'il n'a pas été trouvé
     return gameId
-    local function findGameId()
+    local funtion findGameId()
     -- Insérez ici le code pour effectuer la recherche de l'ID de jeu, par exemple :
     local gameId = "3260590327" -- Exemple d'ID trouvé
 
-    -- Retourne l'ID trouvé ou nil s'il n'a pas été trouvé
+    -- Retourne lID trouvé ou nil s'il n'a pas été trouvé
     return gameId
  end
   end
 
 
 
--- Fonction pour exécuter le script en fonction de l'ID de jeu
-local function executeScript(gameId)
-    if gameId == "3260590327" then
-        -- Si l'ID du jeu est 3260590327, exécutez le script suivant
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/frrp9/auto-farm-drems-/main/Tower_Defense_Simulator.lua"))()
-    elseif gameId == "2753915549" then
+-- Fonction pour exécuter lescript en fonction de l'ID de jeu
+local function executeScript(gam== "3260590327" then
+        -- Si l'D du jeu est 3260599" then
         -- Si l'ID du jeu est 2753915549, exécutez le script suivant
-        loadstring(game:HttpGet("https://pastebin.com/raw/HH8YbxzC"))()
-    else
-        -- Si l'ID du jeu n'a pas été trouvé, affichez un message d'erreur
-        print("ID de jeu non valide")
+        loadtring(game:HttpGeta palide")
     end
 end
 
--- Fonction principale pour lancer le chargement et l'exécution de script
+-- Fonction pnt et l'exécution de script
 local function main()
-    showLoadingMenu()
+    showLoadingMnu()
     local gameId = findGameId()
     if gameId then
         executeScript(gameId)
